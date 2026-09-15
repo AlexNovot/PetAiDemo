5e80a817c9d07837256ce1115f6b26fa70335fcb
Aleksandr Novotelnov <aleksvander@yandex.ru>
2026-09-16 03:21:08 +0400

    тест

--- src/main/java/org/aleksvander/petaidemo/petaidemo/service/OwnerService.java
+++ src/main/java/org/aleksvander/petaidemo/petaidemo/service/OwnerService.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class OwnerService {
 
     private final OwnerRepository ownerRepository;
